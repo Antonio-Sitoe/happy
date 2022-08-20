25d943f9c7582a19de70d18eae46f5ff9a42e985
Luan Henrique <65981953+LuanHMA@users.noreply.github.com>
2022-08-20 17:51:19 -0300

    Finalizando o HTML da Home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,19 +1,35 @@
 import { Container } from '../styles/home';
+import { FiArrowRight } from 'react-icons/fi';
 import logo from '../assets/logo.svg';
+import ilustration from '../assets/ilustrationHome.svg';
+import Link from "next/link";
+
 
 function Home(){
     return(
         <Container>
             <div className="content">
-                <div className="left-content">
+                <header>
                     <img src={logo} alt="Logotipo (Happy)"/>
 
-                    <h1>Leve felicidade para o mundo</h1>
-                    <small>Visite orfanatos e mude o dia de muitas crianças.</small>
-                </div>
+                    <div className="location">
+                        <strong>Rio do Sul</strong>
+                        <span>Santa Catarina</span>
+                    </div>
+                </header>
 
-                <div className="right-content">
+                <div className="box">
+                    <main className="left-content">
+                        <h1>Leve felicidade para o mundo</h1>
+                        <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+                    </main>
 
+                    <div className="right-content">
+                        <img src={ilustration} alt="Ilustração de crianças alegres"/>
+                        <Link href="/"> 
+                            <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
+                        </Link>
+                    </div>
                 </div>
             </div>
         </Container>
