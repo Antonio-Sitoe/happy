136c4b80f1366cd99596d447744c2ff003a128e6
Luan Henrique <65981953+LuanHMA@users.noreply.github.com>
2022-08-22 15:33:31 -0300

    Estilizando a home
    -Ajustei o tamanho do layout para Web
    -Modifiquei o tamanho e peso das fontes
    -Modifiquei os espaçamentos

--- src/styles/home.ts
+++ src/styles/home.ts
@@ -1,12 +1,132 @@
 import styled from "styled-components";
 
+// export const Container = styled.div`
+//   display: grid;
+//   place-items:center;
+//   background: linear-gradient(329.54deg, #29b6d1 0%, #00c7c7 100%);
+//   height: 100vh;
+//   width: 100%;
+  
+//   .content{
+//     position: relative;
+//     width: 80%;
+//     margin: 20px auto;
+//     max-width: 1100px;
+//     border: 1px solid black;
+
+//     header,.box{
+//       display: flex;
+//       justify-content:space-between;
+//       align-items:center;
+//     }
+
+//     header{
+//       .location{
+//         display: flex;
+//         flex-direction: column;
+//         align-items: flex-end;
+//         color: #fff;
+//       }
+//     }
+//     .box{
+//       .left-content{
+//         margin-left: 30px;
+
+//         h1{
+//           font-size: 76px;
+//           font-weight: 900;
+//           line-height: 70px;
+//         }
+//       }
+//     }
+   
+//   }
+// `;
+
+
 export const Container = styled.div`
+  display: grid;
+  place-items:center;
   background: linear-gradient(329.54deg, #29b6d1 0%, #00c7c7 100%);
   height: 100vh;
   width: 100%;
 
-  .content {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
+  div.content{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 85%;
+    max-width: 1200px;
+    position: relative;
+
+    header{
+      display: flex;
+      align-items:center;
+      justify-content:space-between;
+
+      img{
+        left: -40px;
+      }
+      .location{
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        color: #fff;
+        font-size: 24px;
+        line-height: 34px;
+      }
+    }
+    .box{
+      display: grid;
+      grid-template-columns: 1fr 2fr;
+
+      .left-content{
+        display: flex;
+        flex-direction: column;
+        justify-content:center;
+        align-items:flex-start;
+        color: #fff;
+
+        h1{
+          font-size: 76px;
+          font-weight: 900;
+          line-height: 70px;
+          margin-bottom: 0;
+        }
+        p{
+          max-width: 350px;
+          margin-top: 20px;
+          font-size: 24px;
+          line-height: 34px;
+        }
+
+      }
+      .right-content{
+        display: flex;
+        justify-content: center;
+        align-items:flex-end;
+        position: relative;
+        top: -60px;
+        
+        svg{
+          position: absolute;
+          bottom:0;
+          right:150px;
+          width: 60px;
+          height: 60px;
+          padding: 10px;
+          background: #FFD666;
+          border-radius: 15px;
+          cursor:pointer;
+        }
+
+        img{
+          top: -45px;
+          left: -15px;
+          overflow:initial;
+        }
+      }
+    }
+
   }
 `;
