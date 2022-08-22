cdb4f321121d0922acb61abef78a64811d18f5bd
Antonio Sitoe <72309855+Antonio-Sitoe@users.noreply.github.com>
2022-08-22 21:02:01 +0200

    Atualizacao na font

--- src/styles/home.ts
+++ src/styles/home.ts
@@ -6,7 +6,7 @@ import styled from "styled-components";
 //   background: linear-gradient(329.54deg, #29b6d1 0%, #00c7c7 100%);
 //   height: 100vh;
 //   width: 100%;
-  
+
 //   .content{
 //     position: relative;
 //     width: 80%;
@@ -39,19 +39,42 @@ import styled from "styled-components";
 //         }
 //       }
 //     }
-   
+
 //   }
 // `;
 
+export const NavLink = styled.a`
+  border-radius: 0.9rem;
+  max-width: 60px;
+  width: 100%;
+  height: 60px;
+  background: #ffd666;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: 0.6s;
+  svg {
+    color: #252525;
+    width: 30px;
+    height: 30px;
+  }
 
+  &:hover {
+    svg {
+      stroke: #15c3d6;
+    }
+    background: #96feff;
+  }
+`;
 export const Container = styled.div`
   display: grid;
-  place-items:center;
+  place-items: center;
   background: linear-gradient(329.54deg, #29b6d1 0%, #00c7c7 100%);
   height: 100vh;
   width: 100%;
 
-  div.content{
+  div.content {
     display: flex;
     flex-direction: column;
     justify-content: space-between;
@@ -59,15 +82,15 @@ export const Container = styled.div`
     max-width: 1200px;
     position: relative;
 
-    header{
+    header {
       display: flex;
-      align-items:center;
-      justify-content:space-between;
+      align-items: center;
+      justify-content: space-between;
 
-      img{
+      img {
         left: -40px;
       }
-      .location{
+      .location {
         display: flex;
         flex-direction: column;
         align-items: flex-end;
@@ -76,57 +99,43 @@ export const Container = styled.div`
         line-height: 34px;
       }
     }
-    .box{
+    .box {
       display: grid;
       grid-template-columns: 1fr 2fr;
 
-      .left-content{
+      .left-content {
         display: flex;
         flex-direction: column;
-        justify-content:center;
-        align-items:flex-start;
+        justify-content: center;
+        align-items: flex-start;
         color: #fff;
 
-        h1{
+        h1 {
           font-size: 76px;
           font-weight: 900;
           line-height: 70px;
           margin-bottom: 0;
         }
-        p{
+        p {
           max-width: 350px;
           margin-top: 20px;
           font-size: 24px;
           line-height: 34px;
         }
-
       }
-      .right-content{
+      .right-content {
         display: flex;
         justify-content: center;
-        align-items:flex-end;
+        align-items: flex-end;
         position: relative;
         top: -60px;
-        
-        svg{
-          position: absolute;
-          bottom:0;
-          right:150px;
-          width: 60px;
-          height: 60px;
-          padding: 10px;
-          background: #FFD666;
-          border-radius: 15px;
-          cursor:pointer;
-        }
 
-        img{
+        img {
           top: -45px;
           left: -15px;
-          overflow:initial;
+          overflow: initial;
         }
       }
     }
-
   }
 `;
