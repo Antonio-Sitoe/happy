ecda740b8db1630340ce125ec3a7997dab7e6f4f
Antonio Sitoe <72309855+Antonio-Sitoe@users.noreply.github.com>
2022-08-28 00:50:14 +0200

    update mapht

--- src/Components/Maps/SimpleMaph.tsx
+++ src/Components/Maps/SimpleMaph.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 import styled from "styled-components";
-import { MapContainer, TileLayer, useMap } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
-const SimpleMapStyle = styled.div``;
+const SimpleMapStyle = styled.div`
+height: 600px;
+
+`;
 
 const SimpleMaph = () => {
   return (
     <SimpleMapStyle>
-      {/* <MapContainer></MapContainer> */}
+      <MapContainer  center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </SimpleMapStyle>
   );
 };
