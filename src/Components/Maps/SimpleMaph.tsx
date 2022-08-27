cb4f1656c19896f5a101452f4c49d4e0d023c40f
Antonio Sitoe <72309855+Antonio-Sitoe@users.noreply.github.com>
2022-08-28 00:38:07 +0200

    update

--- src/Components/Maps/SimpleMaph.tsx
+++ src/Components/Maps/SimpleMaph.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import styled from "styled-components";
-import { MapContainer, TileLayer, useMap } from "react-leaflet";
+
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 const SimpleMapStyle = styled.div``;
 
 const SimpleMaph = () => {
   return (
     <SimpleMapStyle>
-      {/* <MapContainer></MapContainer> */}
+   
     </SimpleMapStyle>
   );
 };
