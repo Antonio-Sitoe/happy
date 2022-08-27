5c1886ca8bf6d8da8a78665366b2c6303ae3d2fa
Antonio Sitoe <72309855+Antonio-Sitoe@users.noreply.github.com>
2022-08-28 00:43:37 +0200

    Update dados.tsx

--- src/pages/dados.tsx
+++ src/pages/dados.tsx
@@ -1,91 +1,91 @@
-import React from 'react'
-import { Container } from '../styles/dados'
-import Layault from '../Components/Layault/Layault'
-import { useForm, SubmitHandler  } from "react-hook-form";
+import React from "react";
+import { Container } from "../styles/dados";
+import Layault from "../Components/Layault/Layault";
+import { useForm, SubmitHandler } from "react-hook-form";
+import styled from "styled-components";
 
-type Inputs = { 
-  name: string,
-  sobre: string,
-  whatsappNumber: number,
-  instruction: string,
-  visitHour: number
-}
+type Inputs = {
+  name: string;
+  sobre: string;
+  whatsappNumber: number;
+  instruction: string;
+  visitHour: number;
+};
+
+const Form = styled.form`
+  padding: 2rem;
+`;
 
 const Dados = () => {
-
-  const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
-
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
   return (
-    <Layault title='Adicione um orfanato'>
-        <form>
-          <Container>
-            <section className="section-dados">
+    <Layault title="Adicione um orfanato">
+      <Form>
+        <Container>
+          <section className="section-dados">
+            <h1>Dados</h1>
 
-              <h1>Dados</h1>
+            <div className="field">mapa</div>
 
-              <div className="field">
-                mapa
+            <div className="field">
+              <label htmlFor="name">Nome</label>
+              <input type="text" id="name" />
+            </div>
+
+            <div className="field">
+              <div className="info-sobre">
+                <label htmlFor="sobre">Sobre</label>
+                <small>Máximo de 300 carecteres</small>
               </div>
 
-              <div className="field">
-                <label htmlFor="name">Nome</label>
-                <input type="text" id="name"/>
-              </div>
+              <textarea id="sobre" maxLength={300} />
+            </div>
 
-              <div className="field">
-                <div className="info-sobre">
-                  <label htmlFor="sobre">Sobre</label>
-                  <small>Máximo de 300 carecteres</small>
-                </div>
+            <div className="field">
+              <label htmlFor="whatsappNumber">Número do Whatsapp</label>
+              <input type="number" id="whatsappNumber" />
+            </div>
 
-                <textarea id="sobre" maxLength={300}/>
-              </div>
+            <div className="field">
+              <label htmlFor="photo">Fotos</label>
+              <button>+</button>
+            </div>
+          </section>
 
-              <div className="field">
-                <label htmlFor="whatsappNumber">Número do Whatsapp</label>
-                <input type="number" id="whatsappNumber"/>
-              </div>
+          <section className="section-validacao">
+            <h1>Visitação</h1>
 
-              <div className="field">
-                <label htmlFor="photo">Fotos</label>
-                <button>+</button>
-              </div>
-            </section>
+            <div className="field">
+              <label htmlFor="instruction">Instrução</label>
+              <textarea id="instruction" maxLength={300} />
+            </div>
 
-            <section className="section-validacao">
+            <div className="field">
+              <label htmlFor="visitHour">Horário de visitas</label>
+              {/* aqui e campo tipo texto mesmo */}
+              <input type="text" id="visitHour" />
+            </div>
 
-              <h1>Visitação</h1>
+            <div className="toggle-content">
+              <label htmlFor="checkbox">Atende fim de semana?</label>
+              <input type="checkbox" name="" id="checkbox" />
+            </div>
 
-              <div className="field">
-                <label htmlFor="instruction">Instrução</label>
-                <textarea id="instruction" maxLength={300}/>
-              </div>
-
-              <div className="field">
-                <label htmlFor="visitHour">Horário de visitas</label>
-                <input type="number" id="visitHour"/>
-              </div>
-
-              <div className="toggle-content">
-                <label htmlFor="checkbox">Atende fim de semana?</label>
-                <input type="checkbox" name="" id="checkbox" />
-              </div>
-
-              <div className="field">
-                <input type="submit" value="Confirmar" />
-              </div>
-
-            </section>
-
-
-
-
-          </Container>
-        </form>
+            <div className="field">
+              <input type="submit" value="Confirmar" />
+            </div>
+          </section>
+        </Container>
+      </Form>
     </Layault>
-  )
-}
+  );
+};
 
-export default Dados
+export default Dados;
