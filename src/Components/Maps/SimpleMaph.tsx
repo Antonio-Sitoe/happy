e99b8d282498a61202c83df23ee21b745fc21b4a
Antonio Sitoe <72309855+Antonio-Sitoe@users.noreply.github.com>
2022-08-28 10:04:33 +0200

    Update SimpleMaph.tsx

--- src/Components/Maps/SimpleMaph.tsx
+++ src/Components/Maps/SimpleMaph.tsx
@@ -1,28 +1,26 @@
 import React from "react";
 import styled from "styled-components";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
 
 const SimpleMapStyle = styled.div`
-height: 600px;
-
+  height: 400px;
 `;
 
+  var map = L.map("map").setView([51.505, -0.09], 13);
+
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  L.marker([51.5, -0.09])
+    .addTo(map)
+    .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+    .openPopup();
+
 const SimpleMaph = () => {
-  return (
-    <SimpleMapStyle>
-      <MapContainer  center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
-    </SimpleMapStyle>
-  );
+
+  return <SimpleMapStyle className="map"></SimpleMapStyle>;
 };
 
 export default SimpleMaph;
