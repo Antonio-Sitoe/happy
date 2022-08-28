496bffdbb4347a8829b50b2d7cc5bcb0ff1ce157
Antonio Sitoe <72309855+Antonio-Sitoe@users.noreply.github.com>
2022-08-28 10:25:47 +0200

    Update dados.tsx

--- src/pages/dados.tsx
+++ src/pages/dados.tsx
@@ -1,90 +1,109 @@
-import React, { useState } from 'react'
-import { Container } from '../styles/dados'
-import Layault from '../Components/Layault/Layault'
+import React, { useState } from "react";
+import { Container } from "../styles/dados";
+import Layault from "../Components/Layault/Layault";
 import { useForm, SubmitHandler } from "react-hook-form";
-import { yupResolver } from '@hookform/resolvers/yup';
+import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import styled from "styled-components";
 
 type Inputs = {
-  name: string,
-  about: string,
-  whatsappNumber: number,
-  instruction: string,
-  visitHour: number,
-  weekendService: boolean
-}
+  name: string;
+  about: string;
+  whatsappNumber: number;
+  instruction: string;
+  visitHour: number;
+  weekendService: boolean;
+};
 
 const Form = styled.form`
   padding: 2rem;
+
+  @media (max-width:30rem){
+    padding: 0.6rem;
+  }
 `;
 
-const schema = yup.object({
-  name: yup.string().required("Por favor, informe seu nome!"),
-  about: yup.string().required("Esse campo é obrigatório!"),
-  whatsappNumber: yup.string().min(11, "Digite no minímo 11 caracteres").required("Informe o número do seu whatsapp ou de contato"),
-  instruction: yup.string().required("Esse campo é obrigatório!"),
-  visitHour: yup.string().required("Informe um horário de visita!"),
-  weekendService: yup.boolean()
-
-}).required();
+const schema = yup
+  .object({
+    name: yup.string().required("Por favor, informe seu nome!"),
+    about: yup.string().required("Esse campo é obrigatório!"),
+    whatsappNumber: yup
+      .string()
+      .min(11, "Digite no minímo 11 caracteres")
+      .required("Informe o número do seu whatsapp ou de contato"),
+    instruction: yup.string().required("Esse campo é obrigatório!"),
+    visitHour: yup.string().required("Informe um horário de visita!"),
+    weekendService: yup.boolean(),
+  })
+  .required();
 
 const Dados = () => {
+  const [dataUser, setDataUser] = useState({});
 
-
-  const [ dataUser , setDataUser ] = useState({});
-  
   const {
     register,
     handleSubmit,
     watch,
-    formState: { errors }
+    formState: { errors },
   } = useForm<Inputs>({
-    resolver: yupResolver(schema)
+    resolver: yupResolver(schema),
   });
 
-
-
-  const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
   return (
-
     <Layault title="Adicione um orfanato">
-      <Form  onSubmit={handleSubmit(onSubmit)}>
+      <Form onSubmit={handleSubmit(onSubmit)}>
         <Container>
           <section className="section-dados">
             <h1>Dados</h1>
-
             <div className="field">mapa</div>
 
             <div className="field">
-               <label htmlFor="name">Nome *</label>
-              <input type="text" id="name" />
-                  <input type="text" id="name" {...register("name")} required/>
-              {errors.name && <span className="error-alert">{errors.name?.message}</span>}
+              <label htmlFor="name">Nome</label>
+              <input type="text" id="name" {...register("name")} required />
+              {errors.name && (
+                <span className="error-alert">{errors.name?.message}</span>
+              )}
             </div>
 
             <div className="field">
               <div className="info-sobre">
-
-            <label htmlFor="sobre">Sobre *</label>
+                <label htmlFor="sobre">Sobre</label>
                 <small>Máximo de 300 carecteres</small>
               </div>
 
-                   <textarea id="sobre" maxLength={300} {...register("about")} required/>
-              {errors.about && <span className="error-alert">{errors.about?.message}</span>}
-            </div>
-
-           <div className="field">
-              <label htmlFor="whatsappNumber">Número do Whatsapp *</label>
-              <input type="number" id="whatsappNumber" {...register("whatsappNumber")} required/>
-              {errors.whatsappNumber && <span className="error-alert">{errors.whatsappNumber?.message}</span>}
-
+              <textarea
+                id="sobre"
+                maxLength={300}
+                {...register("about")}
+                required
+              />
+              {errors.about && (
+                <span className="error-alert">{errors.about?.message}</span>
+              )}
             </div>
 
             <div className="field">
-             <label htmlFor="photo"className="inputPhoto">+</label>
-              <input type="file" id="photo"/>
+              <label htmlFor="whatsappNumber">Número do Whatsapp</label>
+              <input
+                type="number"
+                id="whatsappNumber"
+                {...register("whatsappNumber")}
+                required
+              />
+              {errors.whatsappNumber && (
+                <span className="error-alert">
+                  {errors.whatsappNumber?.message}
+                </span>
+              )}
+            </div>
+
+            <div className="field">
+              <label htmlFor="photo" className="inputPhoto">
+                +
+              </label>
+              <input type="file" id="photo" />
             </div>
           </section>
 
@@ -92,22 +111,41 @@ const Dados = () => {
             <h1>Visitação</h1>
             <div className="field">
               <label htmlFor="instruction">Instrução *</label>
-              <textarea id="instruction" maxLength={300} {...register("instruction")} required/>
-              {errors.instruction && <span className="error-alert">{errors.instruction?.message}</span>}
+              <textarea
+                id="instruction"
+                maxLength={300}
+                {...register("instruction")}
+                required
+              />
+              {errors.instruction && (
+                <span className="error-alert">
+                  {errors.instruction?.message}
+                </span>
+              )}
             </div>
 
             <div className="field">
-              <label htmlFor="visitHour">Horário de visitas *</label>
-              <input type="time" id="visitHour " {...register("visitHour")} required/>
-              {errors.visitHour && <span className="error-alert">{errors.visitHour?.message}</span>}
-              <button>+</button>
+              <label htmlFor="visitHour">Horário de visitas</label>
+              <input
+                type="time"
+                id="visitHour "
+                {...register("visitHour")}
+                required
+              />
+              {errors.visitHour && (
+                <span className="error-alert">{errors.visitHour?.message}</span>
+              )}
             </div>
           </section>
 
           <section className="section-validacao">
             <div className="toggle-content">
               <label htmlFor="checkbox">Atende fim de semana?</label>
-              <input type="checkbox" id="checkbox" {...register("weekendService")} />
+              <input
+                type="checkbox"
+                id="checkbox"
+                {...register("weekendService")}
+              />
             </div>
             <div className="field">
               <input type="submit" value="Confirmar" />
